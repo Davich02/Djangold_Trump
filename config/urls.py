8deee2adea509b94ce6d0c4a9a4c4c7f9aeb722b
Davich02 <david.rabotarus@gmail.com>
2026-05-05 19:16:57 +0200

    Added requirements.txt and two types of greetings

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from myapp.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home-page/',greeting),
+    path('<str:name>/',greeting_name), # add name into url
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse,HttpRequest
 
 # Create your views here.
+# Home-page greeting
+def greeting(request:HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello from our first view!")
+
+# Greeting with name
+def greeting_name(request:HttpRequest,name:str) -> HttpResponse:
+    return HttpResponse(f'<h1>Hello , {name}!</h1>')
