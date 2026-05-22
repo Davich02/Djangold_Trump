03959870466fc3f37b6c7623704d8a4141232e7f
Davich02 <david.rabotarus@gmail.com>
2026-05-22 10:48:40 +0200

    added Task, SubTask, Category models with Meta and admin / Почистил от лекционных записей

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from myapp.models import Category, Task, SubTask
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'deadline', 'created_at')
+    list_filter = ('status', 'categories')
+    search_fields = ('title', 'description')
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task', 'status', 'deadline', 'created_at')
+    list_filter = ('status',)
+    search_fields = ('title', 'description')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+status_modes = [
+    ('New', 'New'),
+    ('In progress', 'In progress'),
+    ('Pending', 'Pending'),
+    ('Blocked', 'Blocked'),
+    ('Done', 'Done'),
+]
+
+
+class Task(models.Model):
+    description = models.TextField()
+    title = models.CharField(max_length=200)
+    categories = models.ManyToManyField('Category')
+    status = models.CharField(max_length=20, choices=status_modes, default='New')
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "task_manager_task"
+        ordering = ['-created_at']
+        verbose_name = "Task"
+        verbose_name_plural = 'Tasks'
+        constraints = [
+            models.UniqueConstraint(fields=['title'], name='unique_task_title')
+        ]
+
+    def __str__(self):
+        return self.title
+
+
+class SubTask(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=status_modes, default='New')
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "task_manager_subtask"
+        ordering = ['-created_at']
+        verbose_name = "SubTask"
+        verbose_name_plural = 'SubTasks'
+        constraints = [
+            models.UniqueConstraint(fields=['title'], name='unique_subtask_title')
+        ]
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    class Meta:
+        db_table = 'task_manager_category'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        constraints = [
+            models.UniqueConstraint(fields=['name'], name='unique_category_name')
+        ]
+
+    def __str__(self):
+        return self.name
